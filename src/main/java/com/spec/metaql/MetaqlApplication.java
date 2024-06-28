2f647e847fe8c4bb8056802a5211a1b0ecde0c46
Crirsy <914740874@qq.com>
2024-06-28 16:46:41 +0800

    init

---
+++ src/main/java/com/spec/metaql/MetaqlApplication.java
@@ -0,0 +1,13 @@
+package com.spec.metaql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaqlApplication.class, args);
+    }
+
+}
