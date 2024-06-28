93b5b0425946f2a35b8a9eb4d2b830b01df0b304
Crirsy <914740874@qq.com>
2024-06-28 16:47:28 +0800

    init

---
+++ src/test/java/com/spec/metaql/MetaqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spec.metaql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
